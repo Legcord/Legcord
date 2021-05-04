3a77b0067b7a3d8f711660512b15adebcaab14d6
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2021-05-04 12:08:30 +0200

    Add F5 to reload

--- preload.js
+++ preload.js
@@ -1,10 +1,18 @@
 const customTitlebar = require('custom-electron-titlebar')
+const electronLocalshortcut = require("electron-localshortcut");
+const { remote } = require("electron");
 
 window.addEventListener('DOMContentLoaded', () => {
   new customTitlebar.Titlebar({
     backgroundColor: customTitlebar.Color.fromHex("#2C2F33"),
+    unfocusEffect: true,
+    menu: true,
   });
 
 
 })
+const currentWindow = remote.getCurrentWindow();
+electronLocalshortcut.register(currentWindow, "F5", () => {
+  location.reload();
+});
 require("./utils/capturer.js")
