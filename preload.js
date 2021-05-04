11063cf69e2538947ded342b5b05291dee5a6e51
KayoticCarnige <kayos156official@gmail.com>
2021-05-04 11:38:20 -0400

    Sexierer titlebar and official UI changes

--- preload.js
+++ preload.js
@@ -2,8 +2,61 @@ const customTitlebar = require('custom-electron-titlebar')
 
 window.addEventListener('DOMContentLoaded', () => {
   new customTitlebar.Titlebar({
-    backgroundColor: customTitlebar.Color.fromHex("#2C2F33"),
+    backgroundColor: customTitlebar.Color.fromHex("#202225"),
   });
 
+/**
+ * Utility function to add CSS in multiple passes.
+ * @param {string} styleString
+ */
+ function addStyle(styleString) {
+  const style = document.createElement('style');
+  style.textContent = styleString;
+  document.head.append(style);
+}
 
+addStyle(`
+@import url("https://kckarnige.github.io/femboi_owo/discord-font.css");
+
+.base-3dtUhz, .sidebar-2K8pFh {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+  -ms-flex-direction: column;
+  overflow: hidden;
+  border-top-left-radius: 8px;
+}
+
+div.menubar[role="menubar"] {
+  width: 0px;
+}
+
+.window-title:after {
+  content: "Cord";
+  color: #fff;
+  font-weight: normal;
+  font-size: 12px;
+  font-family: Discordinated;
+}
+
+.window-title:before {
+  content: "ARM";
+  color: #7289da;
+  font-weight: normal;
+  font-size: 12px;
+  font-family: Helvetica, sans-serif;
+}
+
+.window-title {
+  font-size: 0px !important;
+  margin-left: initial !important;
+  transform: translate(10px, 3px) !important;
+}
+
+.titlebar {
+  background: #202225 !important;
+}
+`);
 })
