7a2180f534ab773aa3f4b7ecd88b93cb6dfc9389
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2021-05-05 13:59:03 +0200

    Add a custom context menu.

--- main.js
+++ main.js
@@ -1,9 +1,22 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow, session, Tray, Menu } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, session, Tray, Menu } = require("electron");
+const path = require("path");
+const contextMenu = require("electron-context-menu");
 require("v8-compile-cache");
 require("./utils/updater");
-let mainWindow
+
+contextMenu({
+  prepend: (defaultActions, parameters, browserWindow) => [
+    {
+      label: "Image",
+      // Only show it when right-clicking images
+      visible: parameters.mediaType === "image",
+    },
+  ],
+});
+contextMenu.showSearchWithGoogle = false;
+
+let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
@@ -17,6 +30,7 @@ function createWindow() {
       nodeIntegration: false,
     },
   });
+
   var appIcon = new Tray(__dirname + "/discord.ico");
   mainWindow.webContents.userAgent =
     "Mozilla/5.0 (X12; FreeBSD x86) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36"; //fake useragent
@@ -59,7 +73,6 @@ function createWindow() {
     mainWindow.hide();
   });
 
-
   mainWindow.on("show", function () {
     //temporary fix
     try {
@@ -68,19 +81,17 @@ function createWindow() {
       console.error(error);
       // expected output: TypeError: appIcon is not a function
     }
-
   });
 }
-
-
-
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
-  session.defaultSession.loadExtension(`${require('electron').app.getAppPath()}/goosemod/`)
+  createWindow();
+  session.defaultSession.loadExtension(
+    `${require("electron").app.getAppPath()}/goosemod/`
+  );
   session
     .fromPartition("some-partition")
     .setPermissionRequestHandler((webContents, permission, callback) => {
@@ -89,20 +100,18 @@ app.whenReady().then(() => {
       if (permission === "notifications") {
         // Approves the permissions request
         callback(true);
-      
       }
     });
-  app.on('activate', function () {
+  app.on("activate", function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
 
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
-
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
