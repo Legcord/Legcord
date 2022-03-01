bab60a4f1b63fd75592a00cb78e0ab2256232993
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2022-03-01 16:57:05 +0100

    Update tray.ts

--- src/tray.ts
+++ src/tray.ts
@@ -22,7 +22,7 @@ app.whenReady().then(() => {
       label: "Support Discord Server",
       click: function () {
         mainWindow.show();
-        mainWindow.loadURL("https://discord.gg/F25bc4RYDt");
+        mainWindow.loadURL("https://discord.gg/pZtWQBFjk6");
       },
     },
     {
