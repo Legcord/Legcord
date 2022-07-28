16400adfa9e60db0b6feb5c3bd6dc9c6b5917549
KayoticCarnige <32397453+kckarnige@users.noreply.github.com>
2022-07-27 20:46:00 -0400

    Resolved a dumb issue
    My dumbass not knowing the "echo" command existed smh

--- fixwarn.js
+++
@@ -1,4 +0,0 @@
-console.log('***************************************************')
-console.log('Make sure to run "npm run format" before commiting!')
-console.log('***************************************************')
-console.log(' ')
