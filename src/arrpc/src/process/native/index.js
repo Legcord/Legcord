e2fc0c6401a1b06df9d14ac4e4125a64ee2ef897
aeongdesu <osu@kakao.com>
2022-12-10 18:40:14 +0900

    Update arrpc to 3.0.0 (#284)

---
+++ src/arrpc/src/process/native/index.js
@@ -0,0 +1,2 @@
+const win32 = require("./win32.js");
+module.exports = {win32};
---
+++ src/arrpc/src/process/native/win32.js
@@ -0,0 +1,21 @@
+const {exec} = require("child_process");
+
+module.exports = {
+    getProcesses: () =>
+        new Promise((res) =>
+            exec(`wmic process get ProcessID,ExecutablePath /format:csv`, (e, out) => {
+                res(
+                    out
+                        .toString()
+                        .split("\r\n")
+                        .slice(2)
+                        .map((x) => {
+                            const parsed = x.trim().split(",").slice(1).reverse();
+                            parsed[0] = parseInt(parsed[0]) || parsed[0]; // pid to int
+                            return parsed;
+                        })
+                        .filter((x) => x[1])
+                );
+            })
+        )
+};
