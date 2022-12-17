ac3fd9c6991f6ca9afcdd1ffbfaedede68a11e08
Christopher Snowhill <kode54@gmail.com>
2022-12-16 20:30:04 -0800

    Fix environment access for Linux Wayland patches

--- src/main.ts
+++ src/main.ts
@@ -28,10 +28,10 @@ if (!app.requestSingleInstanceLock()) {
     crashReporter.start({uploadToServer: false, extra: {settingsFile}});
 
     if (process.platform == "linux") {
-        if (process.env.$XDG_SESSION_TYPE == "wayland") {
+        if (process.env.XDG_SESSION_TYPE == "wayland") {
             console.log("Wayland specific patches applied.");
             app.commandLine.appendSwitch("ozone-platform=wayland");
-            if (process.env.$XDG_CURRENT_DESKTOP == "GNOME") {
+            if (process.env.XDG_CURRENT_DESKTOP == "GNOME") {
                 app.commandLine.appendSwitch("enable-features=UseOzonePlatform,WaylandWindowDecorations");
             } else {
                 app.commandLine.appendSwitch("enable-features=UseOzonePlatform");
