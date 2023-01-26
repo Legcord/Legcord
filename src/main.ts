519a82767dd8fe9fa381d4f3f357a87d0b23cd9e
unlwly <unlwly@proton.me>
2023-01-26 18:55:09 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -25,19 +25,15 @@ if (!app.requestSingleInstanceLock()) {
 } else {
     // Your data now belongs to CCP
     crashReporter.start({uploadToServer: false});
-    if (process.env.USE_WAYLAND == "0") {
-        console.log("Wayland patches disabled.");
-    } else {
-        if (process.platform == "linux") {
-            if (process.env.XDG_SESSION_TYPE == "wayland") {
-                console.log("Wayland specific patches applied.");
-                app.commandLine.appendSwitch("ozone-platform=wayland");
-                if (process.env.XDG_CURRENT_DESKTOP == "GNOME") {
-                    app.commandLine.appendSwitch("enable-features=UseOzonePlatform,WaylandWindowDecorations");
-                } else {
-                    app.commandLine.appendSwitch("enable-features=UseOzonePlatform");
-                }
-            }
+    if (process.platform.toLowerCase() === "linux" && process.env.XDG_SESSION_TYPE?.toLowerCase() === "wayland") {
+        app.commandLine.appendSwitch("enable-features=WebRTCPipeWireCapturer");
+        console.log("Wayland detected, using PipeWire for video capture.");
+        if (process.env.USE_WAYLAND === "0") {
+            console.log("Wayland backend disabled.");
+        } else {
+            console.log("Using native Wayland, not Xwayland. Disable with USE_WAYLAND=0 if you find issues.");
+            app.commandLine.appendSwitch("ozone-platform=wayland");
+            app.commandLine.appendSwitch("enable-features=UseOzonePlatform,WaylandWindowDecorations");
         }
     }
 
