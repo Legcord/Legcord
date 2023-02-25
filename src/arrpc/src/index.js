f936382830a0287e32a6edfd99e0380b3ab5d06f
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2023-02-25 21:37:45 +0100

    Fix arRPC

--- src/arrpc/src/index.js
+++ src/arrpc/src/index.js
@@ -2,7 +2,7 @@ const rgb = (r, g, b, msg) => `\x1b[38;2;${r};${g};${b}m${msg}\x1b[0m`;
 const log = (...args) => console.log(`[${rgb(88, 101, 242, "arRPC")}]`, ...args);
 
 log("arRPC v3.1.0 ArmCord");
-const {RPCServer} = require("./server.js");
+const RPCServer = require("./server.js");
 const {mainWindow} = require("../../../ts-out/window.js");
 
 async function run() {
