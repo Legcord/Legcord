c52eff5fb089000f29c49c18166bc3b38ae01712
Angelos Bouklis <53124886+ArjixWasTaken@users.noreply.github.com>
2023-11-30 13:25:58 +0200

    fix aRPC bridge

--- src/content/js/rpc.js
+++ src/content/js/rpc.js
@@ -1,87 +1,92 @@
-window.addEventListener(
-    "load",
-    (() => {
-        let Dispatcher,
-            lookupAsset,
-            lookupApp,
-            apps = {};
+window.addEventListener("load", async () => {
+  let Dispatcher = undefined;
+  let lookupAsset = undefined;
+  let lookupApp = undefined;
+  let apps = {};
 
-        ArmCordRPC.listen(async (data) => {
-            msg = data; //already parsed
-            //console.log(msg);
+  const wpRequire = window.webpackChunkdiscord_app.push([ [Symbol()], {}, (x) => x ]);
+  const cache = wpRequire.c;
+  window.webpackChunkdiscord_app.pop();
 
-            if (!Dispatcher) {
-                const wpRequire = window.webpackChunkdiscord_app.push([[Symbol()], {}, (x) => x]);
-                const cache = wpRequire.c;
-                window.webpackChunkdiscord_app.pop();
+  for (const id in cache) {
+    let mod = cache[id].exports;
+    if (typeof mod !== "object") continue;
 
-                for (const id in cache) {
-                    let mod = cache[id].exports;
-                    for (const prop in mod) {
-                        const candidate = mod[prop];
-                        if (candidate && candidate.register && candidate.wait) {
-                            Dispatcher = candidate;
-                            break;
-                        }
-                    }
-                    if (Dispatcher) break; // make sure to exit outer loop as well
-                }
+    let candidates;
+    try {
+      candidates = Object.values(mod);
+    } catch {
+      continue;
+    }
 
-                const factories = wpRequire.m;
-                for (const id in factories) {
-                    if (factories[id].toString().includes("getAssetImage: size must === [number, number] for Twitch")) {
-                        const mod = wpRequire(id);
+    for (const candidate of candidates) {
+      if (candidate && candidate.register && candidate.wait) {
+        Dispatcher = candidate;
+        break;
+      }
+    }
+  }
 
-                        // fetchAssetIds
-                        const _lookupAsset = Object.values(mod).find(
-                            (e) => typeof e === "function" && e.toString().includes("APPLICATION_ASSETS_FETCH_SUCCESS")
-                        );
-                        if (_lookupAsset)
-                            lookupAsset = async (appId, name) => (await _lookupAsset(appId, [name, undefined]))[0];
-                    }
-                    if (lookupAsset) break;
-                }
+  const factories = wpRequire.m;
+  for (const id in factories) {
+    if (
+      factories[id]
+        .toString()
+        .includes("getAssetImage: size must === [number, number] for Twitch")
+    ) {
+      const mod = wpRequire(id);
 
-                for (const id in factories) {
-                    if (factories[id].toString().includes("APPLICATION_RPC")) {
-                        const mod = wpRequire(id);
+      // fetchAssetIds
+      const _lookupAsset = Object.values(mod).find(
+        (e) =>
+          typeof e === "function" &&
+          e.toString().includes("APPLICATION_ASSETS_FETCH_SUCCESS"),
+      );
+      if (_lookupAsset)
+        lookupAsset = async (appId, name) =>
+          (await _lookupAsset(appId, [name, undefined]))[0];
+    }
+    if (lookupAsset) break;
+  }
 
-                        // fetchApplicationsRPC
-                        const _lookupApp = Object.values(mod).find(
-                            (e) => typeof e === "function" && e.toString().includes(",coverImage:")
-                        );
-                        if (_lookupApp)
-                            lookupApp = async (appId) => {
-                                let socket = {};
-                                await _lookupApp(socket, appId);
-                                return socket.application;
-                            };
-                    }
-                    if (lookupApp) break;
-                }
-            }
+  for (const id in factories) {
+    if (factories[id].toString().includes("APPLICATION_RPC")) {
+      const mod = wpRequire(id);
 
-            if (msg.activity?.assets?.large_image)
-                msg.activity.assets.large_image = await lookupAsset(
-                    msg.activity.application_id,
-                    msg.activity.assets.large_image
-                );
-            if (msg.activity?.assets?.small_image)
-                msg.activity.assets.small_image = await lookupAsset(
-                    msg.activity.application_id,
-                    msg.activity.assets.small_image
-                );
+      // fetchApplicationsRPC
+      const _lookupApp = Object.values(mod).find(
+        (e) => typeof e === "function" && e.toString().includes(",coverImage:"),
+      );
+      if (_lookupApp)
+        lookupApp = async (appId) => {
+          let socket = {};
+          await _lookupApp(socket, appId);
+          return socket.application;
+        };
+    }
+    if (lookupApp) break;
+  }
 
-            // prevent errors when activity is null and let activity stop
-            if (msg.activity) {
-                const appId = msg.activity.application_id;
-                if (!apps[appId]) apps[appId] = await lookupApp(appId);
+  ArmCordRPC.listen(async (msg) => {
+    if (msg.activity) {
+      if (msg.activity?.assets?.large_image)
+        msg.activity.assets.large_image = await lookupAsset(
+          msg.activity.application_id,
+          msg.activity.assets.large_image,
+        );
+      if (msg.activity?.assets?.small_image)
+        msg.activity.assets.small_image = await lookupAsset(
+          msg.activity.application_id,
+          msg.activity.assets.small_image,
+        );
 
-                const app = apps[appId];
-                if (!msg.activity.name) msg.activity.name = app.name;
-            }
+      const appId = msg.activity.application_id;
+      if (!apps[appId]) apps[appId] = await lookupApp(appId);
 
-            Dispatcher.dispatch({type: "LOCAL_ACTIVITY_UPDATE", ...msg}); // set RPC status
-        });
-    })()
-);
+      const app = apps[appId];
+      if (!msg.activity.name) msg.activity.name = app.name;
+    }
+
+    Dispatcher.dispatch({ type: "LOCAL_ACTIVITY_UPDATE", ...msg }); // set RPC status
+  });
+});
