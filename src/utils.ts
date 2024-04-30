3a7853200170b48e34c281784ffd0a6df8d1e636
smartfrigde <37928912+smartfrigde@users.noreply.github.com>
2024-04-30 09:36:05 +0200

    feat: add vaapi performance mode

--- src/utils.ts
+++ src/utils.ts
@@ -133,7 +133,8 @@ export async function injectElectronFlags(): Promise<void> {
     // SOFTWARE.
     const presets = {
         performance: `--enable-gpu-rasterization --enable-zero-copy --ignore-gpu-blocklist --enable-hardware-overlays=single-fullscreen,single-on-top,underlay --enable-features=EnableDrDc,CanvasOopRasterization,BackForwardCache:TimeToLiveInBackForwardCacheInSeconds/300/should_ignore_blocklists/true/enable_same_site/true,ThrottleDisplayNoneAndVisibilityHiddenCrossOriginIframes,UseSkiaRenderer,WebAssemblyLazyCompilation --disable-features=Vulkan --force_high_performance_gpu`, // Performance
-        battery: "--enable-features=TurnOffStreamingMediaCachingOnBattery --force_low_power_gpu" // Known to have better battery life for Chromium?
+        battery: "--enable-features=TurnOffStreamingMediaCachingOnBattery --force_low_power_gpu", // Known to have better battery life for Chromium?
+        vaapi: "--ignore-gpu-blocklist --enable-features=VaapiVideoDecoder --enable-gpu-rasterization --enable-zero-copy --force_high_performance_gpu --use-gl=desktop --disable-features=UseChromeOSDirectVideoDecoder"
     };
     switch (await getConfig("performanceMode")) {
         case "performance":
