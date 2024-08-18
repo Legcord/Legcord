ef5564d38565ee140d9a71f4c999e02cebd9088e
smartfrigde <37928912+smartfrigde@users.noreply.github.com>
2024-08-18 17:35:12 +0200

    format script

--- scripts/fixBundles.js
+++ scripts/fixBundles.js
@@ -1,8 +1,8 @@
 import fs from "fs";
 import path from "path";
 
-const pathToPlugins = path.join(import.meta.dirname,  "..", "ts-out", "plugins");
-console.log(pathToPlugins)
+const pathToPlugins = path.join(import.meta.dirname, "..", "ts-out", "plugins");
+console.log(pathToPlugins);
 fs.readdirSync(pathToPlugins).forEach((file) => {
     const bundle = fs.readFileSync(`${pathToPlugins}/${file}/plugin.js`, "utf8");
     fs.writeFileSync(`${pathToPlugins}/${file}/plugin.js`, bundle.replace(/.*/, "").substr(1));
