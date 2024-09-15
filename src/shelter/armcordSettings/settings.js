cae14b6fcc1f874e76d147ba93a373ff5aca85d4
verysillycat <cortex@duck.com>
2024-09-14 22:56:08 -0600

    Settings(settings.js): add mutual exclusivity between vencord and equicord

--- src/shelter/armcordSettings/settings.js
+++ src/shelter/armcordSettings/settings.js
@@ -10,18 +10,17 @@ export function set(key, value) {
     isRestartRequired = true;
     if (key == "vencord" && value == true) {
         store.vencord = true;
+        store.equicord = false;
         armcord.settings.setConfig("mods", "vencord");
+        armcord.settings.setConfig("equicord", false);
     } else if (key == "vencord" && value == false) {
         store.vencord = false;
         armcord.settings.setConfig("mods", "none");
-    } else {
-        store.settings[key] = value;
-        console.log(key + ": " + store.settings[key]);
-        armcord.settings.setConfig(key, value);
-    }
-    if (key == "equicord" && value == true) {
+    } else if (key == "equicord" && value == true) {
         store.equicord = true;
+        store.vencord = false;
         armcord.settings.setConfig("mods", "equicord");
+        armcord.settings.setConfig("vencord", false);
     } else if (key == "equicord" && value == false) {
         store.equicord = false;
         armcord.settings.setConfig("mods", "none");
