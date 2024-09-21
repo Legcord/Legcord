4bfd5b9c2220a5e76008b6239b1479ace646080c
Hazel Atkinson <yellowsink@riseup.net>
2024-09-21 13:09:33 +0100

    use shelter injector integration instead of the plugin APIs

--- src/discord/preload/mods/shelter.ts
+++ src/discord/preload/mods/shelter.ts
@@ -1,45 +1,26 @@
 import { ipcRenderer, webFrame } from "electron";
 import type { ModBundle } from "../../../@types/ModBundle.js";
-import { sleep } from "../../../common/sleep.js";
-const requiredPlugins: Record<string, string> = {
+
+const requiredPlugins: Record<string, [string, { isVisible: boolean; allowedActions: Record<string, true> }]> = {
     // "armcord-arrpc": "armcord://plugins/armcordRPC/",
-    "armcord-settings": "armcord://plugins/armcordSettings/",
-    "armcord-screenshare": "armcord://plugins/screenshareQualityFix/",
+    "armcord-settings": [
+        "armcord://plugins/armcordSettings/",
+        { isVisible: false, allowedActions: {} }
+    ],
+    "armcord-screenshare": [
+        "armcord://plugins/screenshareQualityFix/",
+        { isVisible: true, allowedActions: { toggle: true } }
+    ],
 };
 try {
     await ipcRenderer.invoke("getShelterBundle").then(async (bundle: ModBundle) => {
         if (bundle.enabled) {
-            await webFrame.executeJavaScript(bundle.js);
+            await webFrame.executeJavaScript(`(()=>{
+                const SHELTER_INJECTOR_PLUGINS = ${JSON.stringify(requiredPlugins)};
+                ${bundle.js}
+            })()`);
         }
     });
 } catch (e) {
     console.error(e);
 }
-async function addPlugins() {
-    await sleep(5000).then(async () => {
-        for (const plugin in requiredPlugins) {
-            console.log(`${plugin}: ${requiredPlugins[plugin]}`);
-            const js = `
-            async function install() {
-                var installed = shelter.plugins.installedPlugins();
-                if (installed["${plugin}"]) {
-                    window.shelter.plugins.startPlugin("${plugin}");
-                } else {
-                    window.shelter.plugins.addRemotePlugin(
-                        "${plugin}",
-                        "${requiredPlugins[plugin]}"
-                    );
-                    await new Promise(r => setTimeout(r, 2000));
-                    window.shelter.plugins.startPlugin("${plugin}");
-            }}
-            install()
-        `;
-            try {
-                await webFrame.executeJavaScript(js);
-            } catch (_e) {
-                console.log(`Plugin ${plugin} already injected`);
-            }
-        }
-    });
-}
-void addPlugins();
