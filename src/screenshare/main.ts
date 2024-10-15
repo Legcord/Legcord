a7ef1635ecc363a22993115f5d539ab909be4d35
smartfrigde <37928912+smartfrigde@users.noreply.github.com>
2024-10-15 20:35:40 +0200

    fix: use system picker for screenshare on macOS

--- src/screenshare/main.ts
+++ src/screenshare/main.ts
@@ -33,61 +33,65 @@ function showAudioDialog(): boolean {
 }
 
 function registerCustomHandler(): void {
-    session.defaultSession.setDisplayMediaRequestHandler((request, callback) => {
-        console.log(request);
-        void desktopCapturer
-            .getSources({
-                types: ["screen", "window"],
-            })
-            .then((sources) => {
-                if (!sources) return callback({});
-                isDone = false;
-                console.log(sources);
-                if (process.platform === "linux" && process.env.XDG_SESSION_TYPE?.toLowerCase() === "wayland") {
-                    console.log("WebRTC Capturer detected, skipping window creation."); //assume webrtc capturer is used
-                    let options: Streams = { video: sources[0] };
-                    if (sources[0] === undefined) return callback({});
-                    if (showAudioDialog() === true) options = { video: sources[0], audio: getConfig("audio") };
-                    callback(options);
-                } else {
-                    capturerWindow = new BrowserWindow({
-                        width: 800,
-                        height: 600,
-                        title: "Legcord Screenshare",
-                        darkTheme: true,
-                        icon: getConfig("customIcon") ?? path.join(import.meta.dirname, "../", "/assets/desktop.png"),
-                        frame: true,
-                        autoHideMenuBar: true,
-                        webPreferences: {
-                            sandbox: false,
-                            spellcheck: false,
-                            preload: path.join(import.meta.dirname, "screenshare", "preload.mjs"),
-                        },
-                    });
-                    ipcMain.once("selectScreenshareSource", (_event, id: string, name: string, audio: boolean) => {
-                        isDone = true;
-                        console.log(`Audio status: ${audio}`);
-                        capturerWindow.close();
-                        const result = { id, name };
+    session.defaultSession.setDisplayMediaRequestHandler(
+        (request, callback) => {
+            console.log(request);
+            void desktopCapturer
+                .getSources({
+                    types: ["screen", "window"],
+                })
+                .then((sources) => {
+                    if (!sources) return callback({});
+                    isDone = false;
+                    console.log(sources);
+                    if (process.platform === "linux" && process.env.XDG_SESSION_TYPE?.toLowerCase() === "wayland") {
+                        console.log("WebRTC Capturer detected, skipping window creation."); //assume webrtc capturer is used
                         let options: Streams = { video: sources[0] };
-                        switch (process.platform) {
-                            case "win32":
-                            case "linux":
-                                options = { video: result };
-                                if (audio) options = { video: result, audio: getConfig("audio") };
-                                callback(options);
-                                break;
-                            default:
-                                callback({ video: result });
-                        }
-                    });
-                    capturerWindow.on("closed", () => {
-                        if (!isDone) callback({});
-                    });
-                    void capturerWindow.loadFile(path.join(import.meta.dirname, "html", "picker.html"));
-                    capturerWindow.webContents.send("getSources", sources);
-                }
-            });
-    });
+                        if (sources[0] === undefined) return callback({});
+                        if (showAudioDialog() === true) options = { video: sources[0], audio: getConfig("audio") };
+                        callback(options);
+                    } else {
+                        capturerWindow = new BrowserWindow({
+                            width: 800,
+                            height: 600,
+                            title: "Legcord Screenshare",
+                            darkTheme: true,
+                            icon:
+                                getConfig("customIcon") ?? path.join(import.meta.dirname, "../", "/assets/desktop.png"),
+                            frame: true,
+                            autoHideMenuBar: true,
+                            webPreferences: {
+                                sandbox: false,
+                                spellcheck: false,
+                                preload: path.join(import.meta.dirname, "screenshare", "preload.mjs"),
+                            },
+                        });
+                        ipcMain.once("selectScreenshareSource", (_event, id: string, name: string, audio: boolean) => {
+                            isDone = true;
+                            console.log(`Audio status: ${audio}`);
+                            capturerWindow.close();
+                            const result = { id, name };
+                            let options: Streams = { video: sources[0] };
+                            switch (process.platform) {
+                                case "win32":
+                                case "linux":
+                                    options = { video: result };
+                                    if (audio) options = { video: result, audio: getConfig("audio") };
+                                    callback(options);
+                                    break;
+                                default:
+                                    callback({ video: result });
+                            }
+                        });
+                        capturerWindow.on("closed", () => {
+                            if (!isDone) callback({});
+                        });
+                        void capturerWindow.loadFile(path.join(import.meta.dirname, "html", "picker.html"));
+                        capturerWindow.webContents.send("getSources", sources);
+                    }
+                });
+        },
+        { useSystemPicker: true },
+    );
 }
 registerCustomHandler();
