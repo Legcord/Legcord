7eefb8705bf8c33f0555052d9b6d9949c87adda7
MahmodZE <97690050+MahmodZE@users.noreply.github.com>
2026-02-14 14:26:17 -0800

    chore: remove @rollup/plugin-json

--- rolldown.config.js
+++ rolldown.config.js
@@ -1,6 +1,5 @@
 import babel from "@rollup/plugin-babel";
 import esmShim from "@rollup/plugin-esm-shim";
-import json from "@rollup/plugin-json";
 import { defineConfig } from "rolldown";
 import copy from "rollup-plugin-copy";
 
@@ -28,7 +27,6 @@ export default defineConfig([
         ],
         plugins: [
             esmShim(),
-            json(),
             copy({
                 targets: [
                     { src: "src/**/**/*.html", dest: "ts-out/html/" },
@@ -48,7 +46,7 @@ export default defineConfig([
             sourcemap: true,
         },
         external: [...electronExternals, "arrpc"],
-        plugins: [esmShim(), json()],
+        plugins: [esmShim()],
     },
     {
         input: "src/discord/preload/preload.mts",
