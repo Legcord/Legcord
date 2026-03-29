db4b1133a477048f1c7a4363a71c11e184250cd6
beb-on <vincent.gwu@gmail.com>
2026-03-29 09:06:25 -0500

    lint

--- src/shelter/settings/pages/SettingsPage.tsx
+++ src/shelter/settings/pages/SettingsPage.tsx
@@ -2,7 +2,7 @@ import { Show } from "solid-js";
 import type { Settings } from "../../../@types/settings.js";
 import { DropdownItem } from "../components/DropdownItem.jsx";
 import { TextBoxItem } from "../components/TextBoxItem.jsx";
-import { setConfig, toggleMod, isMinWindowsVersion } from "../settings.js";
+import { isMinWindowsVersion, setConfig, toggleMod } from "../settings.js";
 import classes from "./SettingsPage.module.css";
 
 const {
