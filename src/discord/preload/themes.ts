54c9b8d3df67413de9a0ffb3834107c04d0799a9
MahmodZE <97690050+MahmodZE@users.noreply.github.com>
2026-04-03 14:24:45 -0700

    chore: fix implicit 'any' types

--- src/discord/preload/themes.ts
+++ src/discord/preload/themes.ts
@@ -1,10 +1,10 @@
 import { addTheme } from "../../common/dom.js";
 const { ipcRenderer } = require("electron");
 
-ipcRenderer.on("addTheme", (_event, name: string, css: string) => {
+ipcRenderer.on("addTheme", (_event: unknown, name: string, css: string) => {
     if (document.getElementById(name)) return;
     addTheme(name, css);
 });
-ipcRenderer.on("removeTheme", (_event, name: string) => {
+ipcRenderer.on("removeTheme", (_event: unknown, name: string) => {
     document.getElementById(name)!.remove();
 });
