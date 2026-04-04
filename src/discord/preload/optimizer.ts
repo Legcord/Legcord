9a22d51cacf91e7008f1389f8330e8477f66ac0a
Xql.dev <205849173+KirobotDev@users.noreply.github.com>
2026-04-05 05:05:15 +0700

    style: biome formatting

--- src/discord/preload/optimizer.ts
+++ src/discord/preload/optimizer.ts
@@ -3,8 +3,7 @@ type OptimizableFunction<T extends Node> = (child: T) => T;
 const optimize = <T extends Node>(orig: OptimizableFunction<T>) => {
     return function (this: Element, ...args: [Element]): T | number {
         if (typeof args[0]?.className === "string" && args[0].className.includes("activity")) {
-           
-           // fix by xql.dev <@1356430365774053448>
+            // fix by xql.dev <@1356430365774053448>
             setTimeout(() => orig.apply(this, args as unknown as [T]), 100);
             return args[0] as unknown as T;
         }
